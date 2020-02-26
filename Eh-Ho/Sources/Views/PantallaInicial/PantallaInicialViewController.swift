d2cc503daa7f94ee12215b8c2a2dca4abbda81fc
jacobomd <javoluis71@gmail.com>
2020-02-26 12:31:55 +0100

    Commit initial

---
+++ Eh-Ho/Sources/Views/PantallaInicial/PantallaInicialViewController.swift
@@ -0,0 +1,27 @@
+//
+//  PantallaInicialViewController.swift
+//  Eh-Ho
+//
+//  Created by Jacobo Morales Diaz on 26/02/2020.
+//  Copyright © 2020 KeepCoding. All rights reserved.
+//
+
+import UIKit
+
+class PantallaInicialViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    // MARK: - Cycle life
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Pantalla inicial"
+        //self.view.backgroundColor = UIColor.blue
+    }
+
+
+    // MARK: - Navigation
+
+    
+
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Eh-Ho' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Eh-Ho
+  pod 'lottie-ios'
+
+end
